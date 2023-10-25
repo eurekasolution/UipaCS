a0d7f64191e02933daf622abfd22683425e5e5b3
eurekasolution <kjmin@daum.net>
2023-10-25 12:57:37 +0900

    초기 커밋.

---
+++ Program.cs
@@ -0,0 +1,5 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+Console.WriteLine("Hello C# World");
+Console.WriteLine("\n\n");
+Console.WriteLine("End of Program");
